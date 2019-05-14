d2f118dc2dcc6c37c02976f1eeb3ee8be1571902
Luiz Philippe <luiz@phari.solutions>
2019-05-13 23:01:55 -0300

    Atividade de laboratório 3/02

--- lab_3/ex_2/Piece.cpp
+++ lab_3/ex_2/Piece.cpp
@@ -1,6 +1,47 @@
 #include "Piece.hpp"
+#include <utility>
 #include <string>
 #include <cstring>
+#include <vector>
+#include <iostream>
+
+Piece::Piece(std::string symbol, std::string current_position, std::string candidate_position){
+  this->symbol = symbol;
+  this->current_position = Piece::stringToPair(current_position);
+  this->candidate_position = Piece::stringToPair(candidate_position);
+}
+
+void Piece::setNextPosition(std::string candidate_position){
+  this->candidate_position = Piece::stringToPair(candidate_position);
+}
+
+void Piece::checkMovement(){
+  if(Piece::positionPresent(this->possible_movements, this->candidate_position)){
+    std::cout << "Moving from (" << this->current_position.first << this->current_position.second << ") to (" << this->candidate_position.first << this->candidate_position.second << ')' << std::endl;
+    this->current_position = this->candidate_position;
+  }
+  else{
+    std::cout << "I cannot move from (" << this->current_position.first << this->current_position.second << ") to (" << this->candidate_position.first << this->candidate_position.second << ')' << std::endl;
+  }
+}
+
+bool Piece::positionPresent(std::vector<std::pair<char, int>> positions, std::pair<char, int> position){
+  for (std::vector<std::pair<char, int>>::iterator it = positions.begin(); it != positions.end(); ++it) {
+    std::pair<char, int> candidate = *it;
+    if(candidate.first == position.first && candidate.second == position.second){
+      return true;
+    }
+  }
+  return false;
+}
+
+int Piece::numericValue(char letter){
+  return (int)letter - (int)'a' + 1;
+}
+
+char Piece::letterValue(int number){
+  return (char)(number + (int)'a' - 1);
+}
 
 std::pair<char, int> Piece::stringToPair(std::string pair_string){
   std::pair<char, int> position;
@@ -9,55 +50,116 @@ std::pair<char, int> Piece::stringToPair(std::string pair_string){
   return position;
 }
 
-Rook::Rook(std::string symbol, std::string current_position, std::string candidate_position){
-  this->symbol = symbol;
-  this->position = Piece::stringToPair(current_position);
-}
+Rook::Rook(std::string symbol, std::string current_position, std::string candidate_position): Piece(symbol, current_position, candidate_position){}
 
 void Rook::setMovements(){
-
+  this->possible_movements.clear();
+  char letter;
+  int number;
+  for (int i = 1; i < 9; i++) {
+    for (int j = 1; j < 9; j++) {
+      std::pair<char, int> position;
+      letter = Piece::letterValue(i);
+      number = j;
+      if (letter == this->current_position.first && number == this->current_position.second) {
+        continue;
+      }
+      if(letter != this->current_position.first && number != this->current_position.second){
+        continue;
+      }
+      position.first = letter;
+      position.second = number;
+      this->possible_movements.push_back(position);
+    }
+  }
 }
 
 void Rook::printPossibleMoves(){
-
+  std::vector<std::pair<char, int>> movements;
+  movements = this->possible_movements;
+  std::cout << "(Rook) Current Position: (" << this->current_position.first << this->current_position.second << ')' << std::endl;
+  std::cout << "I have " << movements.size() << " legal movements." << std::endl;
+  for (std::vector<std::pair<char, int>>::iterator it = movements.begin(); it != movements.end(); ++it) {
+    std::pair<char, int> candidate_position = *it;
+    std::cout << '(' << this->current_position.first << this->current_position.second << ")->(" << candidate_position.first << candidate_position.second << ')' << std::endl;
+  }
 }
 
-void Rook::checkMovement(){
-
-}
-
-Bishop::Bishop(std::string symbol, std::string current_position, std::string candidate_position){
-  if(symbol.at(0) != 'R') exit(0);
-  this->symbol = symbol;
-  this->position = Piece::stringToPair(current_position);
-}
+Bishop::Bishop(std::string symbol, std::string current_position, std::string candidate_position): Piece(symbol, current_position, candidate_position){}
 
 void Bishop::setMovements(){
-
+  this->possible_movements.clear();
+  char letter;
+  int number, number_variation, letter_variation;
+  for (int i = 1; i < 9; i++) {
+    for (int j = 1; j < 9; j++) {
+      std::pair<char, int> position;
+      letter = Piece::letterValue(i);
+      number = j;
+      if (letter == this->current_position.first && number == this->current_position.second) {
+        continue;
+      }
+      letter_variation = Piece::numericValue(this->current_position.first) - Piece::numericValue(letter);
+      number_variation = this->current_position.second - number;
+      if(number_variation == letter_variation || number_variation == -letter_variation){
+        position.first = letter;
+        position.second = number;
+        this->possible_movements.push_back(position);
+      }
+    }
+  }
 }
 
 void Bishop::printPossibleMoves(){
-
+  std::vector<std::pair<char, int>> movements;
+  movements = this->possible_movements;
+  std::cout << "(Bishop) Current Position: (" << this->current_position.first << this->current_position.second << ')' << std::endl;
+  std::cout << "I have " << movements.size() << " legal movements." << std::endl;
+  for (std::vector<std::pair<char, int>>::iterator it = movements.begin(); it != movements.end(); ++it) {
+    std::pair<char, int> candidate_position = *it;
+    std::cout << '(' << this->current_position.first << this->current_position.second << ")->(" << candidate_position.first << candidate_position.second << ')' << std::endl;
+  }
 }
 
-void Bishop::checkMovement(){
-
-}
-
-Knight::Knight(std::string symbol, std::string current_position, std::string candidate_position){
-  if(symbol.at(0) != 'R') exit(0);
-  this->symbol = symbol;
-  this->position = Piece::stringToPair(current_position);
-}
+Knight::Knight(std::string symbol, std::string current_position, std::string candidate_position): Piece(symbol, current_position, candidate_position){}
 
 void Knight::setMovements(){
-
+  this->possible_movements.clear();
+  char letter;
+  int number;
+  for (int i = 1; i < 9; i++) {
+    for (int j = 1; j < 9; j++) {
+      std::pair<char, int> position;
+      letter = Piece::letterValue(i);
+      number = j;
+      if (letter == this->current_position.first && number == this->current_position.second) {
+        continue;
+      }
+      if(letter == this->current_position.first+2 || letter == this->current_position.first-2){
+        if (number == this->current_position.second+1 || number == this->current_position.second-1) {
+          position.first = letter;
+          position.second = number;
+          this->possible_movements.push_back(position);
+        }
+      }
+      else if(number == this->current_position.second+2 || number == this->current_position.second-2){
+        if (letter == this->current_position.first+1 || letter == this->current_position.first-1) {
+          position.first = letter;
+          position.second = number;
+          this->possible_movements.push_back(position);
+        }
+      }
+    }
+  }
 }
 
 void Knight::printPossibleMoves(){
-
-}
-
-void Knight::checkMovement(){
-
+  std::vector<std::pair<char, int>> movements;
+  movements = this->possible_movements;
+  std::cout << "(Knight) Current Position: (" << this->current_position.first << this->current_position.second << ')' << std::endl;
+  std::cout << "I have " << movements.size() << " legal movements." << std::endl;
+  for (std::vector<std::pair<char, int>>::iterator it = movements.begin(); it != movements.end(); ++it) {
+    std::pair<char, int> candidate_position = *it;
+    std::cout << '(' << this->current_position.first << this->current_position.second << ")->(" << candidate_position.first << candidate_position.second << ')' << std::endl;
+  }
 }
--- lab_3/ex_2/Piece.hpp
+++ lab_3/ex_2/Piece.hpp
@@ -3,43 +3,53 @@
 
 #include <utility>
 #include <string>
+#include <vector>
+#include <iostream>
 
 class Piece{
 	public:
 		std::string symbol;
-		std::pair<char, int> position;
+		std::pair<char, int> current_position;
+		std::pair<char, int> candidate_position;
+		std::vector<std::pair<char, int>> possible_movements;
 
-		static std::pair<char, int> stringToPair(std::string pair_string);
+		Piece(std::string symbol, std::string current_position, std::string candidate_position);
+
 		virtual void setMovements() = 0;
 		virtual void printPossibleMoves() = 0;
-		virtual void checkMovement() = 0;
+		void setNextPosition(std::string candidate_position);
+		void checkMovement();
+		static bool positionPresent(std::vector<std::pair<char, int>> positions, std::pair<char, int> position);
+		static std::pair<char, int> stringToPair(std::string pair_string);
+		static int numericValue(char letter);
+		static char letterValue(int number);
 };
 
 class Rook : public Piece{
 	public:
+
 		Rook(std::string symbol, std::string current_position, std::string candidate_position);
 
 		void setMovements() override;
 		void printPossibleMoves() override;
-		void checkMovement() override;
 };
 
 class Bishop : public Piece{
 	public:
+
 		Bishop(std::string symbol, std::string current_position, std::string candidate_position);
 
 		void setMovements() override;
 		void printPossibleMoves() override;
-		void checkMovement() override;
 };
 
 class Knight : public Piece{
 	public:
+
 		Knight(std::string symbol, std::string current_position, std::string candidate_position);
 
 		void setMovements() override;
 		void printPossibleMoves() override;
-		void checkMovement() override;
 };
 
 #endif
